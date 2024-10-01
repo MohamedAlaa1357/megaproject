12132998fec66127ba081542b1fd03a16a267692
MohamedAlaa1357 <144441154+MohamedAlaa1357@users.noreply.github.com>
2024-10-01 05:09:32 +0300

    gfgsd

--- .github/workflows/Terraform/main.tf
+++ .github/workflows/Terraform/main.tf
@@ -17,5 +17,5 @@ resource "aws_instance" "ansible_host" {
 
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
+  value       = aws_instance.ansible_host.public_ip
 }
