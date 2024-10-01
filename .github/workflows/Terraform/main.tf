9693c1e10f95f8ed204060123c7cda2802c2f414
MohamedAlaa1357 <144441154+MohamedAlaa1357@users.noreply.github.com>
2024-10-01 13:24:10 +0300

    aaasda

--- .github/workflows/Terraform/main.tf
+++ .github/workflows/Terraform/main.tf
@@ -2,7 +2,7 @@
 
 
 resource "aws_instance" "ansible_host" {
-  ami                         = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami                         = "ami-08d8ac128e0a1b91c"  # Amazon Linux 2 AMI
   instance_type               = "t2.micro"
   key_name                    = "deployer-key"
   associate_public_ip_address = true
