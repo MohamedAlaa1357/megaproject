3fd70d768d799af256c972ef092c400c852ee5ff
MohamedAlaa1357 <144441154+MohamedAlaa1357@users.noreply.github.com>
2024-10-01 05:23:07 +0300

    asas

--- .github/workflows/Terraform/main.tf
+++ .github/workflows/Terraform/main.tf
@@ -1,9 +1,10 @@
 
 
+
 resource "aws_instance" "ansible_host" {
   ami                         = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.deployer_key.key_name
+  key_name                    = "deployer-key"
   associate_public_ip_address = true
 
   tags = {
@@ -13,5 +14,5 @@ resource "aws_instance" "ansible_host" {
 
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.ansible_host.public_ip
+  value       = aws_instance..public_ip
 }
